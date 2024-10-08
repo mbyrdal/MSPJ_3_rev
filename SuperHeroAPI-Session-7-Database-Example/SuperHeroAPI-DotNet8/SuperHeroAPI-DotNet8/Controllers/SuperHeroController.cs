3c79acc964cc817c9f3963ede1e9adb9e2c1af43
Mads Byrdal Nielsen <mbyrdal9@gmail.com>
2024-10-08 12:53:27 +0200

    SuperHero API
    .

---
+++ SuperHeroAPI-Session-7-Database-Example/SuperHeroAPI-DotNet8/SuperHeroAPI-DotNet8/Controllers/SuperHeroController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNet8.Data;
+using SuperHeroAPI_DotNet8.Entities;
+
+namespace SuperHeroAPI_DotNet8.Controllers
+{
+
+    /// <summary>
+    /// A 'fat' controller, which means it stores all logic within itself. 
+    /// Usually you'd want to inject a repository or a service that contains all the logic.
+    /// The controller would then just forward requests and responses between a client and a server.
+    /// </summary>
+
+    // Route can be found in Properties->launchSettings.json (localhost:7145/ ...)
+    // In this project, [controller] specifies the name of the controller in question -> SuperHeroController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        // Inject our data context DataContext
+        private readonly DataContext _context;
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Async HTTP GET method that returns a list of all SuperHeroes.
+        /// </summary>
+        /// <returns>A list of all SuperHero objects in a list.</returns>
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            // Access our SuperHeroes table...
+            var heroes = await _context.SuperHeroes.ToListAsync();
+            return Ok(heroes);
+
+            /*
+             * OLD VERSION
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+            */
+
+            // Returns status code 200 OK if successful
+        }
+
+        /// <summary>
+        /// Returns a single SuperHero based on their ID. Route is specified in the HttpGet header.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A superHero object with a unique ID.</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int id)
+        {
+            // Finds hero based on ID.
+            var hero = await _context.SuperHeroes.FindAsync(id);
+
+            if(hero == null)
+            {
+                return NotFound("Hero not found."); // Returns status code 404 not found, bad request would be status code 403.
+            }
+
+            return Ok(hero);
+        }
+
+        /// <summary>
+        /// Adds a new SuperHero to the Database using HTTP POST. Input parameter is a SuperHero class with its specifications.<br/>
+        /// Best practice: use a DTO (Data Transfer Object) as input, where only {Name, FirstName, LastName, Place} properties are necessary.<br/>
+        /// IDs can be generated automatically with another request, which also ensures correct indexing on the SuperHero Database.
+        /// </summary>
+        /// <param name="hero">The hero object details</param>
+        /// <returns>Adds a new SuperHero to the DB.</returns>
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            // This code adds the SuperHero to the Database. 
+            // However, this will NOT save the changes -> Call .SaveChangesAsync().
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        /// <summary>
+        /// Updates an existing hero based on input parameters.
+        /// </summary>
+        /// <param name="hero">SuperHero class object</param>
+        /// <returns>Updated SuperHero instance in the Database.</returns>
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero updatedHero)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(updatedHero.Id);
+            if(dbHero == null)
+            {
+                return NotFound("Hero not found.");
+            }
+
+            dbHero.Name = updatedHero.Name;
+            dbHero.FirstName = updatedHero.FirstName;
+            dbHero.LastName = updatedHero.LastName;
+            dbHero.Place = updatedHero.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        /// <summary>
+        /// Deletes a SuperHero based on their ID.
+        /// </summary>
+        /// <param name="id">Hero ID as integer.</param>
+        /// <returns>Updated database where target SuperHero class object is deleted.</returns>
+        [HttpDelete]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(id);
+
+            // Alternative notation for NULL instance
+            if(dbHero is null)
+            {
+                return NotFound("Hero not found.");
+            }
+
+            _context.SuperHeroes.Remove(dbHero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+    }
+}
---
+++ SuperHeroAPI-Session-7-Database-Example/SuperHeroAPI-DotNet8/SuperHeroAPI-DotNet8/Data/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNet8.Entities;
+
+namespace SuperHeroAPI_DotNet8.Data
+{
+    /// <summary>
+    /// Our database context (DataContext) represents a session with the database that can be used to query and save instances of our entities (SuperHeroes).
+    /// Query and save meaning HTTP requests: GET, POST, UPDATE, DELETE.
+    /// It acts as a bridge between the code and the database. 
+    /// By inheriting from DbContext, we can implement and handle CRUD operations on entities, configure our database connection and model relationships.
+    /// Additionally, LINQ functionality to query is implemented to retrieve data from the DB.
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        // Boiler-plate code
+        // DbContextOptions instance carries configuration information such as connection string, DB provider to use, etc.
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        // Name of the table that contains our SuperHero entries.
+        // Our Database set.
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+        // Our hard-coded connection string. Only created for visibility.
+        // We get this using SMSS or SQL Server.
+        // This is implemented in the appsettings.json file -> see ConnectionStrings section...
+        private readonly string _connectionString = "Server=BYRDAL-DESKTOP\\SQLEXPRESS;Database=SuperHeroDb;Trusted_Connection=True;TrustServerCertificate=true;";
+    }
+}
